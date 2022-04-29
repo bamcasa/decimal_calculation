0e6b4389725ce66e3bc23eebda52894c7fa5f972
bamcasa <combeesang@gmail.com>
2022-04-30 01:54:15 +0900

    plus

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import numpy as np
 
-num1 = "99999"
-num2 = "99999"
+num1 = "12345678"
+num2 = "1235678900"
 
 li1 = list(map(int, num1))
 li2 = list(map(int, num2))
 
 if len(li1) >= len(li2):
-    length = len(li1)
+    length = len(li1) + 1
+    for i in range(length - len(li2)):
+        li2.insert(0, 0)
+    li1.insert(0,0)
 else:
-    length = len(li2)
-
-li1.insert(0, 0)
-li2.insert(0, 0)
+    length = len(li2) + 1
+    for i in range(length - len(li1)):
+        li1.insert(0, 0)
+    li2.insert(0,0)
 
 arr1 = np.array(li1)
 arr2 = np.array(li2)
-
-if len(arr1) >= len(arr2):
-    length = len(arr1)
-else:
-    length = len(arr2)
-
+print(arr1)
+print(arr2)
 arr3 = np.zeros(length, dtype=np.int8)
 
 result = np.zeros(length, dtype=np.int8)
