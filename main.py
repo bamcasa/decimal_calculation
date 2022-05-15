1601ec1e4df52309c4742b7e916049da9274f39c
bamcasa <combeesang@gmail.com>
2022-05-15 17:39:54 +0900

    minus(work in negative)

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import numpy as np
 
 
+def list_to_str(result):
+    result = ' '.join(map(str, result)).replace(" ", "")
+    result = result.lstrip('0')
+    return result
+
+
+def trans_complement(arr,length):
+    arr = list(map(int, arr))
+    for i in range(length - len(arr)):
+        arr.insert(0, 0)
+
+    for i in range(length):
+        arr[i] = 9 - arr[i]
+    arr = list_to_str(arr)
+    arr = plus(arr, "1")
+    return arr
+
+
 def plus(num1, num2):
-    if type(num2) == str:
-        num2 = list(map(int, num2))
-    if type(num1) == str:
-        num1 = list(map(int, num1))
-    li1 = num1
-    li2 = num2
+    li2 = list(map(int, num2))
+    li1 = list(map(int, num1))
 
     if len(li1) >= len(li2):
         length = len(li1) + 1
@@ -41,49 +55,42 @@ def plus(num1, num2):
 
     result = np.flip(result)
     # print(result)
+    result = list_to_str(result)
     return result
 
 
 def minus(num1, num2):
     li2 = list(map(int, num2))
     li1 = list(map(int, num1))
-
-
     if len(li1) >= len(li2):
-        length = len(li1) + 1
-        for i in range(length - len(li2)):
-            li2.insert(0, 0)
-        li1.insert(0, 0)
+        length = len(li1)
     else:
-        length = len(li2) + 1
-        for i in range(length - len(li1)):
-            li1.insert(0, 0)
-        li2.insert(0, 0)
+        length = len(li2)
+
     arr1 = np.array(li1)
     arr2 = np.array(li2)
-    trans_arr = np.zeros(length, dtype=np.int8)
+    trans_arr = trans_complement(arr2,length)
+    # print(num1 ,trans_arr)
+    result = plus(num1, trans_arr)
+    # print(length)
+    # print(len(result))
+    # print(len(li1),len(li2))
+    #
+    # print("변환X " + result)
+    # print("변환O " + trans_complement(result, length))
+    if length == len(result) - 1: #양수
+        result = result[-length:]
+        print("양수")
+    elif length >= len(result): #음수
+        result = "-" + trans_complement(result,length)
+        print("음수")
 
-    for i in range(length):  # trans arr
-
-        if i == 0 or i == length - 1:
-            if arr2[i] == 0:
-                trans_arr[i] = 0
-            else:
-                trans_arr[i] = 10 - arr2[i]
-        else:
-            trans_arr[i] = 9 - arr2[i]
-
-    trans_arr = list(trans_arr)
-    del(li1[0])
-    del(trans_arr[0])
-    result = plus(li1, trans_arr)
-    result[0] = 0
     return result
 
 
-num1 = "4628193"
-num2 = "3198472"
+num1 = "1"
+num2 = "881"
 
 result = minus(num1, num2)
-
+# result = list_to_str(result)
 print(result)
