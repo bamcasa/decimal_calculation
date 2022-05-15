61b0c3cce8e0e2618aec69e8b0fc1a55c945dd5a
bamcasa <combeesang@gmail.com>
2022-05-16 02:59:47 +0900

    rasp

---
+++ mail.py
@@ -0,0 +1,38 @@
+import smtplib
+from datetime import datetime
+from email.mime.text import MIMEText
+from decimal_cal import *
+from numba import jit
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+s.starttls()
+
+s.login('combeeh123@gmail.com', 'rcmdnfjognwfdcgs')
+
+def is_prime(x):
+    i = "2"
+    while True:
+        if i == x:
+            break
+        if list_to_str(mod(x, i)) == "":
+            return False
+
+        i = add(i, "1")
+    return True
+
+i = "2"
+while True:
+    if i == "100":
+        break
+    num = power("2", i)
+    num = subtract(num, "1")
+    if is_prime(num):
+        print(f"{num}, 2^{i}-1")
+        msg = MIMEText(f"{num}, 2^{i}-1")
+        msg['Subject'] = f"메르센소수 찾음 {datetime.today().hour}시 {datetime.today().minute}분 {datetime.today().second}초"
+        s.sendmail("combeeh123@gmail.com", "combeesang@gmail.com", msg.as_string())
+
+    i = add(i, "1")
+
+
